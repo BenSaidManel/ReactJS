59b0bdcaed043e3ae8179b491d7831c007a134b7
BenSaidManel <bensaidmanel42@gmail.com>
2021-10-19 17:11:38 +0100

    commit2

--- src/Exercice3.js
+++ src/Exercice3.js
@@ -5,7 +5,8 @@ export class Exercice3 extends Component {
     constructor (props){
         super(props)
         this.state={
-            data : this.props.data
+            data : this.props.data,
+            filter: ""
         }
     }
 
@@ -17,18 +18,46 @@ export class Exercice3 extends Component {
     
       
     }
-   g
+    handleChange = event => {
+        this.setState({ filter: event.target.value });
+      };
+    
     render() {
         return (
             
             <div >
                 <h1>Exercice n3</h1>
                 
-                    <input  id="search" name="search" type="text"></input>  
-                    {Data.map((value,key)=>{
-                        return <p>value.name</p>
+                    <input  id="search" name="search" type="text" placeholder="search" ></input>  
+                    <select onChange={this.handleChange}>  
+                    <option value="">Filter option</option>  
+                    <option value="gender">gender</option>
+                     <option value="name">name</option>
+                     <option value="age">age</option>
+                      <option value="email">email</option>
+                     <option value="phone">phone</option>
+                     <option value="address">address</option>
+                     <option value="balance">balance</option>
+                     <option value="likes">likes</option>
+                    <option value="rating">rating</option>
+                    </select>
+                    
+                    {Data.map((value,index)=>{
+                        return (
+                            <div>
+                        <p> {value.data}</p>
+                        <p></p>
+
+                        </div>
+
+                        )  
                     }
-                    )}    
+                    )} 
+                    
+                    
+                   
+                       
+                      
             </div>
             
         )
